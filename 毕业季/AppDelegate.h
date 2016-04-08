9726b72c580a26216186fd8fc68c76f62c03ead3
aishuaishuai298513 <1131766972@qq.com>
2016-04-08 13:12:55 +0800

    Initial Commit

---
+++ 毕业季/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  毕业季
+//
+//  Created by ass on 16/4/8.
+//  Copyright © 2016年 ass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 毕业季/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  毕业季
+//
+//  Created by ass on 16/4/8.
+//  Copyright © 2016年 ass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
